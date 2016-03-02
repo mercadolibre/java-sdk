d5c0c1a9912d3e9ef58ab1acadf6362e843f6bf0
Nicolás Agustín Legresti <nicolas.legresti@mercadolibre.com>
2016-03-02 16:36:39 -0300

    Fix auth urls

--- src/main/java/com/mercadolibre/sdk/Meli.java
+++ src/main/java/com/mercadolibre/sdk/Meli.java
@@ -25,17 +25,17 @@ public class Meli {
 	 */
 	public static enum AuthUrls {
 		MLA("https://auth.mercadolibre.com.ar"), // Argentina 
-		MLB("https://auth.mercadolibre.com.br"), // Brasil
+		MLB("https://auth.mercadolivre.com.br"), // Brasil
 		MCO("https://auth.mercadolibre.com.co"), // Colombia
 		MCR("https://auth.mercadolibre.com.cr"), // Costa Rica
 		MEC("https://auth.mercadolibre.com.ec"), // Ecuador
-		MLC("https://auth.mercadolibre.com.cl"), // Chile
+		MLC("https://auth.mercadolibre.cl"), // Chile
 		MLM("https://auth.mercadolibre.com.mx"), // Mexico
 		MLU("https://auth.mercadolibre.com.uy"), // Uruguay
 		MLV("https://auth.mercadolibre.com.ve"), // Venezuela
 		MPA("https://auth.mercadolibre.com.pa"), // Panama
 		MPE("https://auth.mercadolibre.com.pe"), // Peru
-		MPT("https://auth.mercadolibre.com.pt"), // Prtugal
+		MPT("https://auth.mercadolivre.pt"), // Prtugal
 		MRD("https://auth.mercadolibre.com.do"); // Dominicana
 		
 		private String value;
