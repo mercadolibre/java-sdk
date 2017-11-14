2b1e58b42ce24b9c44572ab6bee4b7d1569d7bb9
cernadasjuan <cernadasjuan43@gmail.com>
2017-11-14 11:08:54 -0300

    code format

--- src/main/java/com/mercadolibre/sdk/AuthorizationFailure.java
+++ src/main/java/com/mercadolibre/sdk/AuthorizationFailure.java
@@ -1,14 +1,15 @@
 package com.mercadolibre.sdk;
 
 public class AuthorizationFailure extends Exception {
+
     private static final long serialVersionUID = 8688100047490895706L;
 
     public AuthorizationFailure(String message) {
-	super(message);
+        super(message);
 
-    }     
+    }
 
     public AuthorizationFailure(Throwable cause) {
-	super(cause);
+        super(cause);
     }
 }
